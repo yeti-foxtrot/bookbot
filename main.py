c798bf1f2c3e41db8171c06f9be9226c46fe8ace
yeti-foxtrot <eric.sanden33@gmail.com>
2025-06-20 10:01:35 -0600

    Count words function added

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+
+def get_book_text(filepath):
+
+    with open(filepath) as f:
+              file_contents = f.read()
+              return file_contents
+
+def get_book_words(file_contents):
+      list = file_contents.split()
+      num_words = len(list)
+      print(f"{num_words} words found in the document")
+
+def main():
+       
+    x = get_book_text("books/frankenstein.txt")
+    get_book_words(x)
+
+main()
